c6579b802bdacc3661dcaf60db6151bff754bab5
ManuCiao <mn.sabatino@gmail.com>
2016-11-23 15:57:20 +0000

    set up natural language processing to let SCLAKKME  understand the meaning of a question

--- bin/run.js
+++ bin/run.js
@@ -5,13 +5,16 @@ require('dotenv').config();
 const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
+const server = http.createServer(service);
+
+const witToken = process.env.WIT_TOKEN;
+const witClient = require('../server/witClient')(witToken);
 
 const slackToken = process.env.SLACK_TOKEN;
 const slackLogLevel = 'verbose';
 
-const server = http.createServer(service);
 
-const rtm = slackClient.init(slackToken, slackLogLevel);
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
 slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
--- server/slackClient.js
+++ server/slackClient.js
@@ -1,24 +1,50 @@
 'use strict';
 const RtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
+let nlp = null;
 
-function handlerOnAuthenticated(rtmStartData){
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+function handlerOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 }
+
 function addAuthenticatedHandler(rtm, handler) {
-  rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
 }
 
-module.exports.init = function slackClient(token, logLevel){
-  const rtm = new RtmClient(token, {logLevel: logLevel});
-  addAuthenticatedHandler(rtm, handlerOnAuthenticated);
-  return rtm;
+function handlerOnMessage(message) {
+  
+    nlp.ask(message.text, (err, res) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        if (!res.intent) {
+            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+        } else if (res.intent[0].value == 'time' && res.location) {
+            return rtm.sendMessage(`Sorry, I don't yet know the time in ${res.location[0].value}`, message.channel);
+        } else {
+            console.log(res);
+            return rtm.sendMessage("Sorry, I don't know what you are talking about.", message.channel);
+        }
+
+        rtm.sendMessage('Sorry, I did not understand.', message.channel, function messageSent() {
+            // optionally, you can supply a callback to execute once the message has been sent
+        });
+    });
+}
+
+
+module.exports.init = function slackClient(token, logLevel, nlpClient) {
+    rtm = new RtmClient(token, {
+        logLevel: logLevel
+    });
+    nlp = nlpClient;
+    addAuthenticatedHandler(rtm, handlerOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handlerOnMessage);
+    return rtm;
 };
 
 module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-// rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-//   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-// });
-//
-// rtm.start();
